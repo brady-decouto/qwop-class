c32c87a6e67995032e7fe270293bb6e2063bd510
Brady DeCouto <bradydecouto@gmail.com>
2026-07-28 16:07:07 -0400

    Forward results to Google Sheets for permanent storage

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const port = process.env.PORT || 3000
 const path = require('path')
 const fs = require('fs')
 
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzEdzWNCkrciBgPrI93zAcYPbAOmi0TyuYqN9KX5FvW4TRJwfBW3WVKaxM1VAbyqW4RQw/exec'
+
 app.use(express.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.post('/log', (req, res) => {
+app.post('/log', async (req, res) => {
     const { name, score, time } = req.body
     const line = `${new Date().toISOString()},${name},${score},${time}\n`
+
     fs.appendFileSync(path.join(__dirname, 'results.csv'), line)
+
+    try {
+        await fetch(GOOGLE_SHEET_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, score, time })
+        })
+    } catch (err) {
+        console.log('Failed to forward to Google Sheet:', err)
+    }
+
     res.sendStatus(200)
 })
 
